3ad52580559ffbb772ee4b96b8f1217b58665b05
seiyamiyaoka <seiyamiyaoka@gmail.com>
2017-08-22 12:09:20 +0900

    add books state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
+  }
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+  })
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -21,16 +27,16 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                {/* 
+                {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
+
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
                 <input type="text" placeholder="Search by title or author"/>
-                
+
               </div>
             </div>
             <div className="search-books-results">
