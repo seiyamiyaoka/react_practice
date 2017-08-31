bb7d75a842deaf0ccfc36a493fc02d54e1215d76
seiyamiyaoka <seiyamiyaoka@gmail.com>
2017-08-31 19:31:10 +0900

    modify selected value

--- src/Book.js
+++ src/Book.js
@@ -11,8 +11,8 @@ const Book = (props) => {
               <div className="book-top">
                 <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}} > </div>
                 <div className="book-shelf-changer">
-                  <select onChange={(event) => props.onSelectBook(book, event.target.value)}>
-                    <option value="none">Move to...</option>
+                  <select value={book.shelf} onChange={(event) => props.onSelectBook(book, event.target.value)}>
+                    <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
--- src/SearchBook.js
+++ src/SearchBook.js
@@ -50,7 +50,7 @@ class SearchBook extends Component {
                     <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}} > </div>
                     <div className="book-shelf-changer">
                       <select onChange={(event) => this.props.update(book, event.target.value, book)}>
-                        <option value="none">Move to...</option>
+                        <option value="none" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
                         <option value="read">Read</option>
